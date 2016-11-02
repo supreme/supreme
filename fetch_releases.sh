44cd93fdf19fa8b03588d4d0aa533b97a5431936
supreme <sandrews.r@gmail.com>
2016-11-02 12:24:00 -0400

    AUTO UPDATE

--- fetch_releases.sh
+++ fetch_releases.sh
@@ -4,5 +4,5 @@ echo "Fetching new music..."
 python3 scrapers/scraper.py
 git add *
 git commit -m "AUTO UPDATE"
-git push origin master
+git push https://supreme:123rofl123@github.com/supreme/supreme.git master
 echo "Done!"
