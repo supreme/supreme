437093f6fe4791dc03a7149980c1c63bc8d1a872
Stephen Andrews <sandrews.r@gmail.com>
2016-11-02 12:35:15 -0400

    Delete fetch_releases.sh

--- fetch_releases.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Fetching new music..."
-python3 scrapers/scraper.py
-git add *
-git commit -m "AUTO UPDATE"
-git push https://supreme:123rofl123@github.com/supreme/supreme.git master
-echo "Done!"
